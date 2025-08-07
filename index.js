45ca220075617b7b1c9f96f29210c6abedc92296
Ayush Singh <singh07082005@gmail.com>
2025-08-08 00:29:07 +0530

    Initial commit of Dragon vs Dino game

---
+++ index.js
@@ -0,0 +1,67 @@
+score=0;
+cross=true 
+audiogo=new Audio("gameover.mp3")
+audio=new Audio("music.mp3")
+function sound(){
+setTimeout(() => {
+  audio.play();
+}, 1000);
+}
+document.onkeydown= function(e){
+    console.log("Key code is :",e.keyCode)
+if(e.keyCode==38){
+   dino= document.querySelector(".dino")
+   dino.classList.add("animateDino")
+   setTimeout(() => {
+    dino.classList.remove("animateDino")
+   },  700);
+}
+   if(e.keyCode==39){
+    dino=document.querySelector(".dino")
+    dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue("left"))
+    dino.style.left=dinoX+112+"px"
+   }
+   if(e.keyCode==37){
+    dino=document.querySelector(".dino")
+    dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue("left"))
+    dino.style.left=dinoX-112+"px"
+   }
+}
+ setInterval(() => {
+    dino=document.querySelector(".dino")
+    gameover=document.querySelector(".gameover")
+    obstacle=document.querySelector(".obstacle")
+    dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue("left"))
+    dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue("top"))
+    ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("left"))
+    oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue("top"))
+    offsetX=Math.abs(dx-ox);
+    offsetY=Math.abs(dy-oy);
+    if(offsetX<73 && offsetY<52){
+       gameover.innerHTML="Game Over - Reload to Play Again"
+        obstacle.classList.remove("obstacleAni");
+        audiogo.play()
+        setTimeout(() => {
+            audiogo.pause()
+            audio.pause()
+        }, 1000);
+    }
+
+    else if(cross && offsetX<145){
+        score+=1
+        updateScore(score)
+        cross=false
+        setTimeout(() => {
+            cross=true;
+        }, 1000)
+        setTimeout(() => {
+            aniDur=parseFloat(window.getComputedStyle(obstacle,null).getPropertyValue("animation-duration"))
+            newDur=aniDur-0.1
+            obstacle.style.animationDuration=newDur+"s"
+        }, 500)
+       
+    }
+ }, 100);
+ function updateScore(score){
+    document.getElementById("scorecount").innerText="Your Score:"+score
+ }
